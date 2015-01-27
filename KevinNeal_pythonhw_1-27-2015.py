50a9b9683fafe89d5d6bd29b1aed8d76a48d335d
kevinthenaturalist <kevinmneal@gmail.com>
2015-01-27 08:58:57 +0000

    added note on getting division working properly in python 2.7

--- KevinNeal_pythonhw_1-27-2015.py
+++ KevinNeal_pythonhw_1-27-2015.py
@@ -2,6 +2,7 @@
 #homework: Python for Biologists examples on pp 38-40, due 1-27-2015
 
 from __future__ import division
+#use this to ensure python 2.7 knows how to do division
 
 #calculating AT content:
 #ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT
